08d8fe652a5745fd956146a466b6595bcca44dad
Rakib Hossain <35700333+rakiblegends@users.noreply.github.com>
2023-09-12 19:08:27 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll,ll> p64;
+typedef vector<ll> v64;
+typedef vector<p64> vp64;
+ll mod = 1e9+7;
+const ll N = 1000005;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define pb push_back
+#define fi first
+#define se second
+#define nn "\n"
+#define inf 2e18
+#define setbit(x,k) (x|= (1LL<<k))
+#define checkbit(x,k) ((x>>k)&1)
+#define clearbit(x,k) (x&= ~(1LL<<k))
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+
+
+void solve(){
+    ll n,m,temp,ans=0,i,j,k,a,b,c;
+    
+}
+int main()
+{
+    fast_cin();
+    ll t = 1;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        solve();
+    }
+    return 0;
+}
