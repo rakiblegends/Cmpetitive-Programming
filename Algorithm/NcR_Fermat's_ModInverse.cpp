e73b3bb821bb39bb2a954c17c5e05555f9c09282
Rakib Hossain <35700333+rakiblegends@users.noreply.github.com>
2024-01-05 20:48:56 +0600

    Update NcR_Fermat's_ModInverse.cpp

--- Algorithm/NcR_Fermat's_ModInverse.cpp
+++ Algorithm/NcR_Fermat's_ModInverse.cpp
@@ -1,32 +1,31 @@
 #include<bits/stdc++.h>
 #define ll long long int
+ll mod = 1e9+7;
 using namespace std;
 
-ll power(ll x,ll y,ll p){
+ll power(ll x,ll y){
 	ll res = 1; // Initialize result
-	x = x % p; 
+	x = x % mod; 
 	while (y > 0){
-		if (y & 1) res = (res * x) % p;
+		if (y & 1) res = (res * x) % mod;
 		y = y >> 1;
-		x = (x * x) % p;
+		x = (x * x) % mod;
 	}
 	return res;
 }
 // Returns n^(-1) mod p
-ll modInverse(ll n,ll p){
-	return power(n, p - 2, p);
+ll modInverse(ll n){
+	return power(n, mod - 2);
 }
 
 // Returns nCr % p using Fermat's little theorem.
-ll nCrModPFermat(ll n,ll r,ll p){
+ll nCrModPFermat(ll n,ll r){
 	if (n < r) return 0;
 	if (r == 0) return 1;
 	ll fac[n + 1];
 	fac[0] = 1;
-	for (int i = 1; i <= n; i++) fac[i] = (fac[i - 1] * i) % p;
-	return (fac[n] * modInverse(fac[r], p) % p
-			* modInverse(fac[n - r], p) % p)
-		% p;
+	for (int i = 1; i <= n; i++) fac[i] = (fac[i - 1] * i) % mod;
+	return (fac[n] * modInverse(fac[r]) % mod * modInverse(fac[n - r]) % mod) % mod;
 }
 
 int main(){
