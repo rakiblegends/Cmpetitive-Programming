007c8c4c24cb896d73bd0a283beb42fb3a9a96f1
Rakib Hossain <35700333+rakiblegends@users.noreply.github.com>
2024-02-29 11:35:10 +0600

    Create magimal_sum_subsegmets.cpp

---
+++ Algorithm/magimal_sum_subsegmets.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll,ll> p64;
+typedef vector<ll> v64;
+typedef vector<p64> vp64;
+ll mod = 1e9+7;
+const ll N = 500005;
+double eps = 1e-12;
+ll dr[] = {1,-1,0,0};
+ll dc[] = {0,0,1,-1};
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define pb push_back
+#define fi first
+#define se second
+#define nn "\n"
+#define inf 2e18
+#define setbit(x,k) (x|= (1LL<<k))
+#define checkbit(x,k) ((x>>k)&1)
+#define clearbit(x,k) (x&= ~(1LL<<k))
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+
+//Finding subsegments with the maximal sum
+
+
+ll n;
+
+struct dt{
+    ll sum, pref, suff, ans;
+};
+
+dt sg[4*N];
+
+dt make_data(ll val) {
+    dt res;
+    res.sum = val;
+    res.pref = res.suff = res.ans = max(0ll, val);
+    return res;
+}
+
+dt combine(dt l, dt r) {
+    dt res;
+    res.sum = l.sum + r.sum;
+    res.pref = max(l.pref, l.sum + r.pref);
+    res.suff = max(r.suff, r.sum + l.suff);
+    res.ans = max(max(l.ans, r.ans), l.suff + r.pref);
+    return res;
+}
+
+
+void build(v64 &a, ll v,ll tl,ll tr){
+    if (tl == tr) {
+        sg[v] = make_data(a[tl]);
+    } else {
+        ll tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        sg[v] = combine(sg[v*2],sg[v*2+1]);
+    }
+}
+
+
+dt query(ll v,ll tl,ll tr,ll l,ll r){
+    if (l > r) 
+        return make_data(0); //Return 0 for sum, inf for min, -inf for max, 0 for gcd.
+    if (l == tl && r == tr) {
+        return sg[v];
+    }
+    ll tm = (tl + tr) / 2;
+    return combine(query(v*2, tl, tm, l, min(r, tm)), query(v*2+1, tm+1, tr, max(l, tm+1), r));
+}
+
+
+void update(ll v,ll tl,ll tr,ll pos,ll new_val) {
+    if (tl == tr){ 
+        sg[v] = make_data(new_val);
+    } else {
+        ll tm = (tl + tr) / 2;
+        if (pos <= tm)
+            update(v*2, tl, tm, pos, new_val);
+        else
+            update(v*2+1, tm+1, tr, pos, new_val);
+
+        sg[v] = combine(sg[v*2],sg[v*2+1]);
+    }
+}
+
+
+void solve(ll t){
+    ll m,temp,ans=0,i,j,k,b,c,u,v;
+
+    cin>>n>>k;
+    v64 arr(n+1);
+
+    forn(i,n){
+        cin>>arr[i+1];
+    }
+    build(arr,1,1,n);
+    while(k--){
+        ll op, idx, new_val,l,r;
+        cin>>op;
+        if(op==1){
+            cin>>idx>>new_val;
+            update(1,1,n,idx,new_val);
+        }else{
+            cin>>l>>r;
+            cout << query(1,1,n,l,r).ans << nn;
+        }
+    }
+}
+int main()
+{
+    fast_cin();
+    ll t = 1;
+    // cin >> t;
+    for(int it=1;it<=t;it++) {
+        solve(it);
+    }
+    return 0;
+}
